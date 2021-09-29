9a690a27b5364c05545d3c3fb835bf4d162479d8
Vamshi589 <88495207+Vamshi589@users.noreply.github.com>
2021-09-29 16:18:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function updateTime() {
+    var dateTime = new Date();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
+    var seconds = dateTime.getSeconds();
+    var am_or_pm = document.getElementById("am-or-pm");
+
+    // if (hours >= 12) {
+    //     am_or_pm.innerHTML = "PM";
+    // }
+    // else {
+    //     am_or_pm.innerHTML = "AM";
+    // }
+
+    if (hours == 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        am_or_pm = "PM";
+    }
+
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+setInterval(updateTime, 1000);
